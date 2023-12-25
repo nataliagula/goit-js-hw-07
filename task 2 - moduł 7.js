8d16e7d93b170b50203103cec1a75238d9ade1c6
nataliagula <144623671+nataliagula@users.noreply.github.com>
2023-12-25 18:23:45 +0100

    zadanie 2 - moduł 7

---
+++ task 2 - moduł 7.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const ingredientsList = document.getElementById("ingredients");
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+
+  listItem.textContent = ingredient;
+
+  listItem.classList.add("item");
+
+  ingredientsList.appendChild(listItem);
+});
